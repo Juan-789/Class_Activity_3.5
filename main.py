f1ef1c2a56bbfb9cf1c60a431180deb1a15c8cc6
Juan-789 <79994699+Juan-789@users.noreply.github.com>
2021-11-22 12:58:46 -0500

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,9 @@ Write a program that asks a user for two integers. Print "YES" if they're both o
 print("Give me two integers, and I will determine wether both are odd or not ")
 firstInteger = int(input(" Enter the first integer. "))
 secondInteger = int(input("Enter the second Integer. "))
+#asks user for integers
 if firstInteger % 2 == 1 and secondInteger % 2 == 1:
   print ("They are both odd ")
 else:
   print("They aren't both odds")
+#Compares integers to see if they are integers
