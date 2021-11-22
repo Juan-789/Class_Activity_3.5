e2a06de126d02773b30c3210d0afe353712b713f
JuanMarulanda4 <79994699+Juan-789@users.noreply.github.com>
2021-11-22 17:55:45 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+"""
+Write a program that asks a user for two integers. Print "YES" if they're both odd and print "NO" otherwise. Hint use AND or OR
+"""
+print("Give me two integers, and I will determine wether both are odd or not ")
+firstInteger = int(input(" Enter the first integer. "))
+secondInteger = int(input("Enter the second Integer. "))
+if firstInteger % 2 == 1 and secondInteger % 2 == 1:
+  print ("They are both odd ")
+else:
+  print("They aren't both odds")
